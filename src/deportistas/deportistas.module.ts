5b7a3bf0d30691801ee9248f593a8ca75f8e3304
redundante3452 <ahernandez429@soyudemedellin.edu.co>
2026-09-09 13:15:18 -0500

    ✨ feat: importar HttpExternoModule en DeportistasModule

--- src/deportistas/deportistas.module.ts
+++ src/deportistas/deportistas.module.ts
@@ -4,9 +4,10 @@ import { DeportistasController } from './controllers/deportistas.controller';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Deportista } from './entities/deportista.entity';
+import { HttpExternoModule } from '../common/http-externo/http-externo.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Deportista])],
+  imports: [TypeOrmModule.forFeature([Deportista]), HttpExternoModule],
   controllers: [DeportistasController],
   providers: [DeportistasService],
 })
