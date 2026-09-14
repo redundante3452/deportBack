3d3acfecbaecce0f25095427358a603727b8499c
redundante3452 <ahernandez429@soyudemedellin.edu.co>
2026-09-14 16:19:59 -0500

    🔧 chore: importar CacheDistribuidaModule en DeportistasModule

--- src/deportistas/deportistas.module.ts
+++ src/deportistas/deportistas.module.ts
@@ -6,9 +6,14 @@ import { DeportistasV2Controller } from './controllers/deportistas-v2.controller
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Deportista } from './entities/deportista.entity';
 import { HttpExternoModule } from '../common/http-externo/http-externo.module';
+import { CacheDistribuidaModule } from '../common/cache-distribuida/cache-distribuida.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Deportista]), HttpExternoModule],
+  imports: [
+    TypeOrmModule.forFeature([Deportista]),
+    HttpExternoModule,
+    CacheDistribuidaModule,
+  ],
   controllers: [DeportistasController, DeportistasV2Controller],
   providers: [DeportistasService],
 })
